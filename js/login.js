25a796b56b078562dd8e83d03a2c0f29f725cb82
abdulla-1996 <mdabdulla1997@gmail.com>
2026-03-08 16:41:04 +0600

    trying to sole problem in live home page which is showing 404

--- js/login.js
+++ js/login.js
@@ -12,7 +12,7 @@ document.getElementById('login-btn').addEventListener('click', function(){
         //4. 3-1 true ::>>alert> homepage
         alert("login success");
         // window.location.replace("/home.html")
-        window.location.assign("/home.html")
+        window.location.assign("./home.html")
     }
     else{
         //4. 3-1 false ::>>alert> return
